de2a28d2222286ad26cf8ae287f1c1287355dd67
nikgrinding <nikgrinding@gmail.com>
2025-10-23 03:19:46 +0530

    lab-10

--- lab-10-vercel/api/about.js
+++ lab-10-vercel/api/about.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-    const filePath = path.join(__dirname, '../public/about.html');
+    const filePath = path.join(__dirname, '../views/about.html');
     fs.readFile(filePath, 'utf-8', (err, data) => {
         if (err) {
             console.error('Error reading about.html:', err.message);
+            res.setHeader('Content-Type', 'text/html');
             res.status(200).send(
                 '<h1>About Us</h1><p>Could not load about page right now</p>'
             );
         } else {
+            res.setHeader('Content-Type', 'text/html');
             res.status(200).send(data);
         }
     });
--- lab-10-vercel/api/index.js
+++ lab-10-vercel/api/index.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-    const filePath = path.join(__dirname, '../public/index.html');
+    const filePath = path.join(__dirname, '../views/index.html');
     fs.readFile(filePath, 'utf-8', (err, data) => {
         if (err) {
             console.error('Error reading index.html:', err.message);
+            res.setHeader('Content-Type', 'text/html');
             res.status(200).send(
                 '<h1>Home Page</h1><p>Could not load home page right now</p>'
             );
         } else {
+            res.setHeader('Content-Type', 'text/html');
             res.status(200).send(data);
         }
     });
